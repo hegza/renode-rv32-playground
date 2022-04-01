86a52b8295bef3130a86a69bca5ddca68f99a814
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2022-04-01 19:55:16 +0300

    More intelligent script pathing

--- rust/renode.sh
+++ rust/renode.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x #echo on
 
+BASEDIR=$(dirname "$0")
 BINARY=$1
-renode --console -e "include @../scripts/start-rv32imac.resc; set bin @$BINARY; runMacro \$reset; start;"
+renode --console -e "include @$BASEDIR/../scripts/start-rv32imac.resc; set bin @$BINARY; runMacro \$reset; start;"
