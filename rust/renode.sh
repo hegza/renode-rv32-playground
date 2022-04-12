0d170ee1023d616981790cf968dc1cffdff597e6
Henri Lunnikivi <heggggxa@gmail.com>
2022-04-12 12:00:55 +0300

    Update Renode start scripts

--- rust/renode.sh
+++ rust/renode.sh
@@ -2,11 +2,11 @@
 set -x #echo on
 
 # Override platform here
-PLATFORM=rv32imac-vexriscv-plic
+PLATFORM=rv32imac-ibex
 
 BASEDIR=$(dirname "$0")
 BIN=${BIN=$1}
 
 PLATFORM_PATH=../platforms/${PLATFORM=rv32imac-vexriscv}.repl
 
-renode --console -e "set bin @$BIN; set platform_path @$PLATFORM_PATH; include @$BASEDIR/../scripts/start.resc; runMacro \$reset; start;"
+renode --console -e "set bin @$BIN; set platform_path @$PLATFORM_PATH; include @$BASEDIR/../scripts/start.resc"
