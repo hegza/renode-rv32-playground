f38689bc04acb2d145bf9f4787ae0e81e4388eae
Henri Lunnikivi <heggggxa@gmail.com>
2023-01-03 14:53:04 +0200

    Some PLIC experimentation

--- rust/interrupts-playground/src/main.rs
+++ rust/interrupts-playground/src/main.rs
@@ -15,15 +15,11 @@ const CORE_FREQ_MIPS: u32 = 100;
 const CORE_FREQ: u32 = CORE_FREQ_MIPS * 1_000_000;
 
 pub type CLINT = common::core_level_interruptor::CoreLevelInterruptor<0xF001_0000, 1>;
-//const PLIC: *mut usize = 0x50000000 as *mut usize;
-/*const PRIO_BITS: usize = 3;
-pub type PLIC = plic::Plic<0x50000000, PRIO_BITS>;*/
 
 #[entry]
 unsafe fn main() -> ! {
     common::init_uart();
 
-    //sprintln!("MPP: {:?}", mstatus::read().mpp());
     sprintln!("Hello UART!");
 
     // Timer lo should be at 0xF001BFF8
@@ -44,28 +40,16 @@ unsafe fn main() -> ! {
     // CLINT reg[0] |= 0b1 generates the interrupt from CLINT@0 -> CPU@X
     CLINT::raise(0);
 
-    // TODO: Set up a timer too
     sprintln!("Setting timer for 1 second");
     let seconds = 1;
     CLINT::set_time_cmp(0, CLINT::time() + CORE_FREQ as u64 * seconds);
 
     sprintln!("pre-wfi");
     {
+        // Go to sleep
         riscv::asm::wfi();
     }
     sprintln!("post-wfi");
 
-    /*
-    unsafe {
-        // PLIC
-        let hart_id = mhartid::read();
-        sprintln!("{}", hart_id);
-        let threshold = PLIC::get_threshold(hart_id);
-        let irq = PLIC::claim(hart_id).unwrap();
-        let prio = PLIC::get_priority(irq);
-    }
-
-    sprintln!("Bottom code");
-    */
     loop {}
 }
